7ef886c1733e0959b203b2b06b21c511c2911c69
Noah Brenner <noah@noahbrenner.com>
2019-05-25 15:19:00 -0700

    Add knockout ViewModel boilerplate

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,10 +1,28 @@
+import * as ko from 'knockout';
+
 import '../css/styles.scss';
 import html from '../templates/bar.ejs.html';
 import text from '../templates/foo.ejs.txt';
 
-const templateLocals = {
-    content: 'Yay!'
-};
+class ViewModel {
+    public title: ko.Observable<string>;
 
-document.getElementById('output')!.innerText = html(templateLocals);
-document.querySelector('input')!.value = text(templateLocals);
+    public text = ko.pureComputed(() => text(this.templateLocals()));
+    public html = ko.pureComputed(() => html(this.templateLocals()));
+
+    private templateLocals = ko.pureComputed(() => ({
+        content: this.title().trim()
+    }));
+
+    constructor() {
+        this.title = ko.observable('');
+    }
+}
+
+if (document.readyState === 'loading') {
+    ko.applyBindings(new ViewModel());
+} else {
+    document.addEventListener('DOMContentLoaded', () => {
+        ko.applyBindings(new ViewModel());
+    });
+}

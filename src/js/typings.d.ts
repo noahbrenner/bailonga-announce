703a6fe15bfbaa098163055cd41dae242a32b930
Noah Brenner <noah@noahbrenner.com>
2019-05-25 13:37:10 -0700

    Ensure TypeScript declaration files are linted

--- src/js/typings.d.ts
+++ src/js/typings.d.ts
@@ -1,13 +1,18 @@
-// `import` statements break global declarations, but this (hack) works:
-// https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts
-type TemplateFunction = import('ejs').TemplateFunction;
+/*
+ * We want to re-export the `TemplateFunction` type from `ejs`, but if we use an
+ * `import` statement in this file, the types defined here won't be available
+ * in our project as ambient declarations. Instead, we're using TypeScript's
+ * `import()` function, which doesn't have that side-effect.
+ *
+ * https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts
+ */
 
 declare module '*.ejs.html' {
-    const content: TemplateFunction;
-    export default content;
+    const templateFunction: import('ejs').TemplateFunction;
+    export default templateFunction;
 }
 
 declare module '*.ejs.txt' {
-    const content: TemplateFunction;
-    export default content;
+    const templateFunction: import('ejs').TemplateFunction;
+    export default templateFunction;
 }

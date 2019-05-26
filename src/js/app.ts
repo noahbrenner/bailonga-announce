26be7d451b57557ab785fc1fb46fc8903c5eee4c
Noah Brenner <noah@noahbrenner.com>
2019-05-25 18:33:38 -0700

    Implement a draft of the Facebook event template
    Only plain text fields are collected in the form at this point.
    Other fields just have placeholders.

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,21 +1,37 @@
 import * as ko from 'knockout';
 
 import '../css/styles.scss';
-import html from '../templates/bar.ejs.html';
-import text from '../templates/foo.ejs.txt';
+import facebook from '../templates/facebook.ejs.txt';
 
 class ViewModel {
     public title: ko.Observable<string>;
+    public intro: ko.Observable<string>;
+    public dj: ko.Observable<string>;
+    public teacherBeginner: ko.Observable<string>;
+    public teacherIntermediate: ko.Observable<string>;
+    public topicIntermediate: ko.Observable<string>;
+    public cost: ko.Observable<string>;
 
-    public text = ko.pureComputed(() => text(this.templateLocals()));
-    public html = ko.pureComputed(() => html(this.templateLocals()));
+    public facebook = ko.pureComputed(() => facebook(this.templateLocals()));
 
     private templateLocals = ko.pureComputed(() => ({
-        content: this.title().trim()
+        title: this.title().trim(),
+        intro: this.intro().trim(),
+        dj: this.dj().trim(),
+        teacherBeginner: this.teacherBeginner().trim(),
+        teacherIntermediate: this.teacherIntermediate().trim(),
+        topicIntermediate: this.topicIntermediate().trim(),
+        cost: this.cost().trim()
     }));
 
     constructor() {
         this.title = ko.observable('');
+        this.intro = ko.observable('');
+        this.dj = ko.observable('');
+        this.teacherBeginner = ko.observable('');
+        this.teacherIntermediate = ko.observable('');
+        this.topicIntermediate = ko.observable('TBD');
+        this.cost = ko.observable('$7 – $10');
     }
 }
 

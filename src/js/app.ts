1ca4c2992c810044641f0784dec4d025beed7ebd
Noah Brenner <noah@noahbrenner.com>
2019-06-20 12:49:44 -0700

    Implement drop-down menus
    Drop-down menus were added for:
    
    * Music type
    * Beginner lesson topic

--- src/js/app.ts
+++ src/js/app.ts
@@ -16,10 +16,29 @@ class ViewModel {
     public date = ko.observable('');
     public intro = ko.observable('');
     public dj = ko.observable('');
+    public musicType = ko.observable('');
     public teacherBeginner = ko.observable('');
+    public topicBeginner = ko.observable('');
     public teacherIntermediate = ko.observable('');
     public topicIntermediate = ko.observable('');
     public cost = ko.observable('');
+
+    public musicTypeOptions: readonly string[] = [
+        '50/50 Alternative and Traditional',
+        '100% Traditional',
+        '100% Alternative',
+        'First 3/4 traditional, last 1/4 alternative',
+        'Tango/Blues-Fusion alternative mix'
+        // TODO 'Other' (and add a text box)
+    ];
+
+    public topicBeginnerOptions: readonly string[] = [
+        'Week 1: From zero, the very basics.',
+        'Week 2: Musicality',
+        'Week 3: Molinete',
+        'Week 4: Cross System',
+        'Week 5: Bonus Topic TBA'
+    ];
 
     public facebook = ko.pureComputed(() => facebook(this.templateLocals()));
 
@@ -28,13 +47,23 @@ class ViewModel {
         date: formatUTCDate(new Date(this.date())),
         intro: this.intro().trim(),
         dj: this.dj().trim(),
+        musicType: this.musicType(),
         teacherBeginner: this.teacherBeginner().trim(),
+        topicBeginner: this.topicBeginner(),
         teacherIntermediate: this.teacherIntermediate().trim(),
         topicIntermediate: this.topicIntermediate().trim(),
         cost: this.cost().trim()
     }));
 
     constructor() {
+        // Update the default beginner topic whenever the event date changes
+        this.date.subscribe((newDateString) => {
+            const selectedDate = new Date(newDateString);
+            const weekIndex = Math.floor((selectedDate.getUTCDate() - 1) / 7);
+            this.topicBeginner(this.topicBeginnerOptions[weekIndex]);
+        });
+
+        // Initialize default values
         Object.entries(this.getDefaultValues())
             .forEach(([key, val]) => this[key as InputProperty](val));
     }

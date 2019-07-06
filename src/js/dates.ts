c8cd8085b80e827698420c56859815f6966b48d6
Noah Brenner <noah@noahbrenner.com>
2019-07-05 18:18:11 -0700

    Refactor `getNextTuesdayISOString()` in dates.ts

--- src/js/dates.ts
+++ src/js/dates.ts
@@ -28,6 +28,45 @@ export function formatUTCDate(date: Date) {
 }
 
 /**
+ * Return a new Date in UTC time with `localDate`'s year, month, and date
+ */
+function localDateToUTCDate(localDate: Date) {
+    return new Date(Date.UTC(
+        localDate.getFullYear(), localDate.getMonth(), localDate.getDate(),
+    ));
+}
+
+/**
+ * Return the number of days between `utcDate` and the nearest Tuesday
+ * When `utcDate` is itself a Tuesday, 0 is returned
+ *
+ * @param future - Search forward if true, backward if false
+ * When searching backward, the return value is negative or 0
+ */
+export function getDaysTillTuesday(utcDate: Date, future = true) {
+    const TUESDAY = 2;
+    const weekOffset = future ? 7 : -7;
+
+    return (weekOffset + TUESDAY - utcDate.getUTCDay()) % weekOffset;
+}
+
+/**
+ * Return the ISO date string for `refDate` shifted by `offset` days
+ *
+ * > getISOStringWithOffset(new Date('2015-01-06'), 1); // => '2015-01-07'
+ * > getISOStringWithOffset(new Date('2015-01-06'), -1); // => '2015-01-05'
+ */
+export function getISOStringWithOffset(utcDate: Date, offset: number) {
+    const utcResult = new Date(Date.UTC(
+        utcDate.getUTCFullYear(),
+        utcDate.getUTCMonth(),
+        utcDate.getUTCDate() + offset
+    ));
+
+    return utcResult.toISOString().slice(0, 10);
+}
+
+/**
  * Return the ISO date string for the Tuesday after `refDate`
  *
  * > getNextTuesdayISOString('2015-01-05'); // => '2015-01-06'
@@ -40,22 +79,12 @@ export function formatUTCDate(date: Date) {
  * If a `Date`: A Date object in *local* time, e.g. `new Date()`
  */
 export function getNextTuesdayISOString(refDate: string | Date) {
-    const WEEK = 7;
-    const TUESDAY = 2;
+    const utcDate = refDate instanceof Date
+        ? localDateToUTCDate(refDate)
+        : new Date(refDate);
 
-    const utcRef = typeof refDate === 'string'
-        ? new Date(refDate)
-        : new Date(Date.UTC(
-            refDate.getFullYear(), refDate.getMonth(), refDate.getDate()
-        ));
+    // If refDate is a Tuesday itself, we want the *next* Tuesday
+    const offset = getDaysTillTuesday(utcDate) || 7;
 
-    const dateOffset = (WEEK - (utcRef.getUTCDay() - TUESDAY)) % WEEK || WEEK;
-
-    const utcResult = new Date(Date.UTC(
-        utcRef.getUTCFullYear(),
-        utcRef.getUTCMonth(),
-        utcRef.getUTCDate() + dateOffset
-    ));
-
-    return utcResult.toISOString().slice(0, 10);
+    return getISOStringWithOffset(utcDate, offset);
 }

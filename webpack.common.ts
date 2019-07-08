8f26e846ede943f54b2f5d66a8ff7ccba70166e5
Noah Brenner <noah@noahbrenner.com>
2019-07-07 17:42:32 -0700

    Move index.pug into src/html/

--- webpack.common.ts
+++ webpack.common.ts
@@ -86,7 +86,7 @@ export const baseConfig: webpack.Configuration = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             inject: 'head',
-            template: './src/index.pug'
+            template: './src/html/index.pug'
         })
     ],
 

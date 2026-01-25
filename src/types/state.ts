1393300edd78ff08beeccd4b7b012013cdeb650c
Noah Brenner <noah@noahbrenner.com>
2026-01-25 13:33:25 -0800

    Add checkboxes and post text by category for Facebook groups

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,3 +1,5 @@
+import { objectFromEntries } from "../js/utils/objects";
+
 export const VENUE_OPTIONS = [
     "Ballroom",
     "Colonial Room",
@@ -23,6 +25,42 @@ export interface IUpcomingEvent {
     title: string;
 }
 
+export const LOCAL_FACEBOOK_GROUPS = [
+    "Eugene Tango",
+    "Corvallis Tango",
+] as const;
+export const REMOTE_FACEBOOK_GROUPS = [
+    "Alt Tango NW",
+    "US West Coast Tango",
+    "PDX Tango",
+    "Portland Tango",
+] as const;
+export const NON_TANGO_FACEBOOK_GROUPS = [
+    "Bailamos Eugene",
+    "Dance Network Eugene",
+    "Eugene Area Dance Club",
+] as const;
+
+export type FacebookGroupCheckboxState = Record<
+    (
+        | typeof LOCAL_FACEBOOK_GROUPS
+        | typeof REMOTE_FACEBOOK_GROUPS
+        | typeof NON_TANGO_FACEBOOK_GROUPS
+    )[number],
+    boolean
+>;
+
+export const ALL_FACEBOOK_GROUPS = [
+    ...LOCAL_FACEBOOK_GROUPS,
+    ...REMOTE_FACEBOOK_GROUPS,
+    ...NON_TANGO_FACEBOOK_GROUPS,
+] as const;
+
+export const DEFAULT_FB_GROUP_CHECKBOX_STATE: Readonly<FacebookGroupCheckboxState> =
+    objectFromEntries(
+        ALL_FACEBOOK_GROUPS.map((groupName) => [groupName, false])
+    );
+
 export interface IState {
     title: string;
     date: string;
@@ -41,4 +79,5 @@ export interface IState {
     photoCredit: string;
     photoCreditMailchimp: string;
     facebookEventUrl: string;
+    facebookGroupCheckboxState: FacebookGroupCheckboxState;
 }
